201dd96b51a74658f0de15ee3ca1e9497a165e10
annjessil <annmariejessil8@gmail.com>
2025-04-13 18:57:56 -0700

    lwords

--- wordcount/word_count_l.c
+++ wordcount/word_count_l.c
@@ -49,25 +49,69 @@ word_count_t *find_word(word_count_list_t *wclist, char *word) {
 
 word_count_t *add_word_with_count(word_count_list_t *wclist, char *word,
                                   int count) {
-    /* TODO */
-    return NULL;
+    word_count_t *wcExisting = find_word(wclist, word);
+    if (wcExisting != NULL){
+        wcExisting->count = wcExisting->count + count;
+        return wcExisting;
+       
+    } else {
+        word_count_t *newWC = malloc(sizeof(word_count_t));
+        if (newWC == NULL){
+            return NULL; //in case malloc doesnt work
+        }
+
+        newWC->count = count;
+        newWC->word = word; //strdup(word), if having issues
+        list_push_back(wclist, &newWC->elem);
+        return newWC;
+    } 
+
+    
 }
 
 word_count_t *add_word(word_count_list_t *wclist, char *word) {
-    return add_word_with_count(wclist, word, 1);
+    /*
+ * Insert word with count=1, if not already present; increment count if
+ * present. Takes ownership of word.
+ */
+    word_count_t *wc = find_word(wclist, word);
+    if (wc != NULL){
+        wc->count = wc->count + 1;
+        return wc;
+    } else {
+        return add_word_with_count(wclist, word, 1);
+    }
 }
 
 void fprint_words(word_count_list_t *wclist, FILE *outfile) {
-    /* TODO */
+    struct list_elem *wc = list_begin(wclist);
+    for (wc = list_begin(wclist); wc != list_end(wclist); wc = list_next(wc)) {
+        word_count_t *element = list_entry(wc, word_count_t, elem);
+        fprintf(outfile, "%8d\t%s\n", element->count, element->word);
+    }
 }
 
 static bool less_list(const struct list_elem *ewc1,
                       const struct list_elem *ewc2, void *aux) {
-    /* TODO */
+    //if first entry is less than second returns true               
+    word_count_t *elem1 = list_entry(ewc1, word_count_t, elem);
+    word_count_t *elem2 = list_entry(ewc2, word_count_t, elem);
+
+    if (elem1->count < elem2->count){
+        return true;
+    } else if (elem1->count == elem2->count){
+        if(strcmp(elem1->word, elem2->word) < 0){
+            return true;
+        } 
+    } 
+
     return false;
+
 }
 
-void wordcount_sort(word_count_list_t *wclist,
-                    bool less(const word_count_t *, const word_count_t *)) {
-    list_sort(wclist, less_list, less);
+
+void wordcount_sort(word_count_list_t *wclist, bool less(const word_count_t *, const word_count_t *)) {
+    //sorting by word or count??
+    list_sort(wclist, less_list, NULL);
 }
+
